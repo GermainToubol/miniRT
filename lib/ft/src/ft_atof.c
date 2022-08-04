7dc04764064091e0c59f040a56cef936a0b8999d
Germain Toubol <germain.toubol.pro@gmail.com>
2022-08-04 15:22:10 +0200

    correct ft_atof in sign and comma
    Patching ft_atof to take in account the sign after the comma and the
    separating comma (between numbers)

--- lib/ft/src/ft_atof.c
+++ lib/ft/src/ft_atof.c
@@ -17,17 +17,23 @@ float	ft_atof(const char *str)
 	float	nbr;
 	float	exponent;
 	int		i;
+	int		sign;
 
 	nbr = (float) ft_atoi(str);
 	i = 0;
-	while (str[i] && str[i] != '.')
+	sign = 1;
+	while (str[i] && str[i] != '.' && str[i + 1] != ',')
+	{
+		if (str[i] == '-')
+			sign = -1;
 		i++;
+	}
 	exponent = 0.1;
 	if (str[i])
 		i++;
 	while (ft_isdigit(str[i]))
 	{
-		nbr += (str[i] - '0') * exponent;
+		nbr += (str[i] - '0') * exponent * sign;
 		exponent *= 0.1;
 		i++;
 	}
