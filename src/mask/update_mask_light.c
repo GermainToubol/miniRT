ee60ac80f8bc5c7b25e8004439e52244c2377586
Germain TOUBOL <germain.toubol.pro@gmail.com>
2022-08-17 12:19:59 +0200

    Update masks on lights and camera

--- src/mask/update_mask_light.c
+++ src/mask/update_mask_light.c
@@ -6,13 +6,13 @@
 /*   By: gtoubol <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 14:29:31 by gtoubol           #+#    #+#             */
-/*   Updated: 2022/08/16 14:34:21 by gtoubol          ###   ########.fr       */
+/*   Updated: 2022/08/17 11:14:18 by gtoubol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "raster.h"
 #include "scene.h"
 
-void	update_mask_light(t_obj *obj, t_light *light, int n)
+void	update_single_mask_single_light(t_obj *obj, t_light *light, int n)
 {
 	const t_mask_func init_mask[] = {
 		set_sphere_mask,
@@ -22,4 +22,31 @@ void	update_mask_light(t_obj *obj, t_light *light, int n)
 	};
 
 	(*init_mask[obj[n].tag])(obj + n, light, light->mask + n);
+}
+
+void	update_all_masks_single_light(t_scene *scene, t_light *light)
+{
+	int	i;
+
+	i = -1;
+	while (++i < scene->nb_objs)
+		update_single_mask_single_light(scene->obj, light, i);
+}
+
+void	update_single_mask_all_lights(t_scene *scene, int n)
+{
+	int	i;
+
+	i = -1;
+	while (++i < scene->nb_lights)
+		update_single_mask_single_light(scene->obj, scene->light + i, n);
+}
+
+void	update_all_masks_all_lights(t_scene *scene)
+{
+	int	i;
+
+	i = -1;
+	while (++i < scene->nb_lights)
+		update_all_masks_single_light(scene, scene->light + i);
 }
